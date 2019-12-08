b3626861cd75cfbdd70a96c4c39c8cb765e64750
Brandon Morris <brandonmorris@pm.me>
2019-12-08 11:37:48 -0700

    Day 3 part 2 solution and tests

--- dart/nineteen/day3.dart
+++ dart/nineteen/day3.dart
@@ -12,19 +12,22 @@ int main() {
 }
 
 void partOneMain() {
-  Pair closest = findClosestIntersection(lines[0], lines[1]);
+  Point closest = findClosestIntersection(lines[0], lines[1]);
   print('Part 1: ${closest.manhattanDist}');
 }
 
-void partTwoMain() {}
+void partTwoMain() {
+  var res = fewestCombinedSteps(lines[0], lines[1]);
+  print('Part 2: ${res}');
+}
 
-class Pair {
-  int first;
-  int second;
-  Pair(this.first, this.second);
+class Point {
+  final int first;
+  final int second;
+  Point(this.first, this.second);
 
   @override
-  bool operator ==(o) => o is Pair && o.first == first && o.second == second;
+  bool operator ==(o) => o is Point && o.first == first && o.second == second;
 
   @override
   String toString() => "($first, $second)";
@@ -40,10 +43,10 @@ class Pair {
   int get manhattanDist => first.abs() + second.abs();
 }
 
-Pair findClosestIntersection(String firstLine, String secondLine) {
-  Set<Pair> collisions = findCollisions(firstLine, secondLine);
+Point findClosestIntersection(String line1, String line2) {
+  Set<Point> collisions = findCollisions(parseLine(line1), parseLine(line2));
   assert(collisions.length > 0);
-  Pair closest = null;
+  Point closest = null;
   for (var collision in collisions) {
     if (closest == null || closest.manhattanDist > collision.manhattanDist) {
       closest = collision;
@@ -52,16 +55,16 @@ Pair findClosestIntersection(String firstLine, String secondLine) {
   return closest;
 }
 
-Set<Pair> findCollisions(String firstLine, String secondLine) {
-  return parseLine(firstLine).intersection(parseLine(secondLine));
+Set<Point> findCollisions(List<Point> path1, List<Point> path2) {
+  return path1.toSet().intersection(path2.toSet());
 }
 
-// Take in a line of input and return a set of coordinates that the wire
+// Take in a line of input and return a list of coordinates that the wire
 // traverses
-Set<Pair> parseLine(String line) {
+List<Point> parseLine(String line) {
   int row = 0;
   int col = 0;
-  Set<Pair> locations = {};
+  List<Point> locations = [];
   List<String> directions = line.split(',');
   for (var direction in directions) {
     var heading = direction[0];
@@ -84,11 +87,37 @@ Set<Pair> parseLine(String line) {
           print('ERROR! Bad direction $direction');
           break;
       }
-      locations.add(Pair(row, col));
+      locations.add(Point(row, col));
     }
   }
   return locations;
 }
+
+// Returns the smallest sum of steps along two paths to a collision
+int fewestCombinedSteps(String line1, String line2) {
+  var path1 = parseLine(line1);
+  var path2 = parseLine(line2);
+  var collisions = findCollisions(path1, path2);
+  int res = null;
+  for (Point collision in collisions) {
+    var sum = stepsToPoint(path1, collision) + stepsToPoint(path2, collision);
+    if (res == null || sum < res) {
+      res = sum;
+    }
+  }
+  return res;
+}
+
+int stepsToPoint(List<Point> path, Point point) {
+  int steps = path.indexOf(point);
+  if (steps == -1) {
+    throw ArgumentError('Point not found in the path');
+  }
+  // Offset by one to account for not including the origin in the path
+  return steps + 1;
+}
+
+// Tests ---------------------------------------------------------------------
 
 void runAllTests() {
   partOneTests();
@@ -96,26 +125,26 @@ void runAllTests() {
 }
 
 void partOneTests() {
-  test("Test test, 1 2 3", () => expect(2 + 2, equals(4)));
   test("Can parse a line", () {
     var res = parseLine('U3,R2,D1,L1');
-    var expected = {
-      Pair(1, 0),
-      Pair(2, 0),
-      Pair(3, 0),
-      Pair(3, 1),
-      Pair(3, 2),
-      Pair(2, 2),
-      Pair(2, 1)
-    };
+    var expected = [
+      Point(1, 0),
+      Point(2, 0),
+      Point(3, 0),
+      Point(3, 1),
+      Point(3, 2),
+      Point(2, 2),
+      Point(2, 1)
+    ];
     expect(res.length, equals(expected.length));
-    for (var p in expected) {
-      expect(res, contains(p));
+    for (int i = 0; i < res.length; i++) {
+      expect(res[i], equals(expected[i]));
     }
   });
   test("Can find collisions", () {
-    var res = findCollisions('U7,R6,D4,L4', 'R8,U5,L5,D3');
-    final expected = {Pair(5, 6), Pair(3, 3)};
+    var res =
+        findCollisions(parseLine('U7,R6,D4,L4'), parseLine('R8,U5,L5,D3'));
+    final expected = {Point(5, 6), Point(3, 3)};
     expect(res.length, equals(expected.length));
     for (var p in expected) {
       expect(res, contains(p));
@@ -137,4 +166,12 @@ void partOneTests() {
   });
 }
 
-void partTwoTests() {}
+void partTwoTests() {
+  test("Part 2 sample input", () {
+    expect(fewestCombinedSteps('R8,U5,L5,D3', 'U7,R6,D4,L4'), equals(30));
+    expect(
+        fewestCombinedSteps('R75,D30,R83,U83,L12,D49,R71,U7,L72',
+            'U62,R66,U55,R34,D71,R55,D58,R83'),
+        equals(610));
+  });
+}
